33b9ece2e071ad1a6b7b6a3865e387ed5566c5fb
Ricardo Pérez <ricpelo@gmail.com>
2019-06-19 13:45:32 +0200

    Los nombres largos los pone en tamaño más pequeños

--- generar.sh
+++ generar.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/zsh
 
 while read p; do
-    echo $p
+    echo -n $p
     sed -e "s/xxxxx/$p/" diploma.svg > tmp.svg
+    if [ $(expr length $p) -ge 35 ]; then
+        echo -n " ***"
+        sed -e "s/72px/56px/" tmp.svg > tmp2.svg
+        mv -f tmp2.svg tmp.svg
+    fi
     inkscape tmp.svg -A "diplomas/$p.pdf"
     rm tmp.svg
+    echo
 done < empresas.txt
