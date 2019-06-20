47b18d666750388e7ae33285dc4d5b1232060dba
Ricardo Pérez <ricpelo@gmail.com>
2019-06-20 20:35:25 +0200

    Cambios, correcciones y nuevas empresas

--- generar.sh
+++ generar.sh
@@ -3,9 +3,13 @@
 while read p; do
     echo -n $p
     sed -e "s/xxxxx/$p/" diploma.svg > tmp.svg
-    if [ $(expr length $p) -ge 35 ]; then
+    if [ $(expr length $p) -ge 40 ]; then
         echo -n " ***"
-        sed -e "s/72px/56px/" tmp.svg > tmp2.svg
+        sed -e "s/56px/40px/" tmp.svg > tmp2.svg
+        mv -f tmp2.svg tmp.svg
+    elif [ $(expr length $p) -ge 30 ]; then
+        echo -n " ***"
+        sed -e "s/56px/44px/" tmp.svg > tmp2.svg
         mv -f tmp2.svg tmp.svg
     fi
     inkscape tmp.svg -A "diplomas/$p.pdf"
